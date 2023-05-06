6dfdcf9879b0f9028cc569c960e30e75c7f23775
Mario Arturo sierra juarez <mario_asj@outlook.com>
2023-05-06 16:33:59 -0600

    Uso de IF

---
+++ UsoFor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsoFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Uso del For
+            int i; 
+            Console.WriteLine("*******Ejemplo de For*******");
+            Console.WriteLine("Imprime numeros de 1 hasta el 10");
+            for (i = 1; i <= 10; i++)
+                Console.WriteLine(i);
+            Console.ReadLine(); 
+        }
+    }
+}
